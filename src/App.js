ab098001bea8f626edbfc1829fb14707e9a8a710
Student <jsitbon@qcc.cuny.edu>
2019-07-03 16:30:25 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          Hello World!
+           </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check this out. Play around and practice. Make yourself at home.
         </a>
       </header>
     </div>
