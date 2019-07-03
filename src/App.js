b967e90f369774107d93a5531ac7ce979364c5b6
Student <jsitbon@qcc.cuny.edu>
2019-07-03 17:39:10 -0400

    react practice

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-           </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Check this out. Play around and practice. Make yourself at home.
-        </a>
-      </header>
-    </div>
-  );
+class Counter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+    }
+  }
+
+  render() {
+     return {
+      <div className = "container">
+      <div className = "navbar">
+      <div className = "counter">
+      <h1> What do we put here??? :) </h1>
+      <button type="button">Increment</button>
+      <button type="button">Decrement</button>
+      </div>
+      </div>
+    }
+  }
 }
 
-export default App;
+
+
+export default Counter;
